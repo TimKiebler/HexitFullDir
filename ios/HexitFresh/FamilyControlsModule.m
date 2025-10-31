8a3def2a218d544aa6f90efd4ef1df8d528031bc
TimKiebler <tim.kiebler@gmx.de>
2025-10-31 15:52:39 +0100

    working bridge to use the locking functionallty right now coppled to the hexButton on the HomeTab

--- ios/HexitFresh/FamilyControlsModule.m
+++
@@ -1,6 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(FamilyControlsModule, NSObject)
-RCT_EXTERN_METHOD(requestAuthorization:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-@end
