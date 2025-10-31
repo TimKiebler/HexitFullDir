3799d1cba23d65ccabad139dc5db6d31869c33fb
TimKiebler <tim.kiebler@gmx.de>
2025-10-31 11:56:48 +0100

    update App deletions

--- ios/FamilyControlsModule.m
+++ ios/FamilyControlsModule.m
@@ -3,4 +3,10 @@
 @interface RCT_EXTERN_MODULE(FamilyControlsModule, NSObject)
 RCT_EXTERN_METHOD(requestAuthorization:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(lockSelectedApps:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(unlockSelectedApps:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(isBlocking:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 @end
